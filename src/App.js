bc838673f69f7adf390458688ef5f023c0b9b2f9
YienCheng <cye_orange@163.com>
2021-03-03 14:50:16 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import HooksDemo from "./components/HooksDemo";
 import "./App.css";
 import React from "react";
+import useChangeText from "./hooks/useChangeText";
+import useClock from "./hooks/useClock";
 
 function App() {
+  // Change Text
+  const { text, changeText } = useChangeText();
+
+  // Clock
+  const date = useClock(new Date());
+
   return (
-    <div style={{ padding: '10px' }}>
-      <HooksDemo />
+    <div style={{ padding: "10px" }}>
+      <button onClick={changeText}>改变文字</button>
+      <h1>{`Hello, ${text}!`}</h1>
+      <h2>It is {date.toLocaleTimeString()}.</h2>
     </div>
   );
 }
